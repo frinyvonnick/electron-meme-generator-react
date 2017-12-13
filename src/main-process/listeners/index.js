c32ec7f261aca3ea166a9a85312bea00b6821c3a
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-12-13 13:18:49 +0100

    :fire: Remove open file dialog and boilerplate

--- src/main-process/listeners/index.js
+++ src/main-process/listeners/index.js
@@ -11,39 +11,3 @@ ipcMain.on('get-memes', (e) => {
     e.sender.send('memes-sent', memes)
   })
 })
-
-ipcMain.on('open-file-dialog', (event) => {
-  dialog.showOpenDialog({
-    properties: ['openFile'],
-    filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] }]
-  }, (files) => {
-    if (files) {
-      const editWindow = newEditWindow(files[0])
-      editWindow.on('close', () => event.sender.send('selected-files'))
-    }
-  })
-})
-
-const saveDialog = (event, meme, window) => {
-  const options = {
-    title: 'Save the meme',
-    defaultPath: process.env.HOME,
-    filters: [
-      { name: 'Images', extensions: ['jpg', 'png', 'gif'] }
-    ]
-  }
-  dialog.showSaveDialog(options, (filename) => {
-    if (!filename) return
-    const copyStream = fs.createReadStream(meme).pipe(fs.createWriteStream(filename))
-    copyStream.on('finish', () => event.sender.send('saved-file-' + window, filename))
-  })
-}
-
-ipcMain.on('save-from-grid', (event, meme) => saveDialog(event, meme, 'grid'))
-ipcMain.on('save-from-detail', (event, meme) => saveDialog(event, meme, 'detail'))
-
-ipcMain.on('delete-selected-meme', (e, selectedMeme) => {
-  deleteMeme(selectedMeme, () => {
-    e.sender.send('meme-deleted')
-  })
-})
--- src/renderer-process/Grid/Grid.jsx
+++ src/renderer-process/Grid/Grid.jsx
@@ -10,14 +10,12 @@ export class Grid extends Component {
 
   componentWillMount() {
     ipcRenderer.on('memes-sent', this.handleMemesSent)
-    ipcRenderer.on('selected-files', this.getMemes)
 
     this.getMemes()
   }
 
   componentWillUnMount() {
     ipcRenderer.removeListener('memes-sent', this.handleMemesSent)
-    ipcRenderer.removeListener('selected-files', this.getMemes)
   }
 
   handleMemesSent = (e, memes) => {
@@ -26,14 +24,6 @@ export class Grid extends Component {
 
   getMemes() {
     ipcRenderer.send('get-memes', {})
-  }
-
-  onClick = () => {
-    ipcRenderer.send('open-file-dialog')
-  }
-
-  deleteMeme = index => {
-    ipcRenderer.send('delete-selected-meme', this.state.memes[index])
   }
 
   render() {
@@ -54,7 +44,6 @@ export class Grid extends Component {
           className="card"
           key="new-meme"
           id="new-meme"
-          onClick={this.onClick}
         >
           <div className="img"></div>
           <h3><span>New</span></h3>
