45716bbc435bef4b9fe7bd5b5464ba29bbb68746
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-12-13 13:28:32 +0100

    :fire: Remove meme mapping

--- src/renderer-process/Grid/Grid.jsx
+++ src/renderer-process/Grid/Grid.jsx
@@ -1,7 +1,3 @@
-import React, { Component } from 'react'
-import { ipcRenderer } from 'electron'
-import path from 'path'
-
 const defaultMemes = [
   {
     title: 'Victory Baby',
@@ -32,17 +28,3 @@ const defaultMemes = [
     file: 'taken.jpg'
   }
 ]
-
-export class Grid extends Component {
-  render() {
-    return defaultMemes.map(({ title, file }, index) => {
-      const url = path.join(__dirname, '..', '..', 'assets', 'img', 'defaults', file)
-      return (
-        <div key={title} className="card meme">
-          <div className="img" style={{ backgroundImage: `url('${url}')` }}></div>
-          <h3 title={title}><span>{title}</span></h3>
-        </div>
-      )
-    })
-  }
-}
